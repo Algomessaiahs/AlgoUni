a020016ad709fa8a178adf5efda1657ed1740754
Surendar.Rajendiran <Surendar.Rajendiran@INPDY-D-0186>
2023-02-08 21:33:31 -0800

    UI0.1

--- AlgoUni/App_Start/BundleConfig.cs
+++ AlgoUni/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace AlgoUni
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                      "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -19,12 +19,16 @@ namespace AlgoUni
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/Home").Include(
+                         "~/Content/HomeUI/About.css",
+                         "~/Content/HomeUI/Index.css",
+                         "~/Content/HomeUI/Contact.css"));
         }
     }
 }
